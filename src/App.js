f2b8afc7326442ab97e5d99c2f554456cae0d871
brandonlee0405 <brandonlee0405@yahoo.com>
2020-04-25 13:51:07 -0700

    Early build of user login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import headerImage from './img/hon-header.jpg';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 import './App.css';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
+
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
       <h1>My React App</h1>
       <h2>Author: Brandon Lee and Charles Park</h2>
       <img src={headerImage} alt="header" />
+
+      <form className={classes.root} noValidate autoComplete="off">
+        <TextField id="filled-basic" label="Username" variant="filled" />
+      </form>
+
+      <form className={classes.root} noValidate autoComplete="off">
+        <TextField id="filled-basic" label="Password" variant="filled" />
+      </form>
     </div>
   );
 }
