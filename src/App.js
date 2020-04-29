9a54325594f40747724a8ab8518ae78ea903f30b
brandonlee0405 <brandonlee0405@yahoo.com>
2020-04-29 10:22:45 -0700

    Using HashRouter to fix github pages routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import headerImage from './img/hon-header.jpg';
 import SignIn from './components/auth/SignIn';
@@ -7,15 +7,13 @@ import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Navbar />
-        <Switch>
-          <Route path="/signin" component={SignIn} />
-        </Switch>
-        <img src={headerImage} alt="header" />
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <Navbar />
+      <Switch>
+        <Route path="/signin" component={SignIn} />
+      </Switch>
+      <img src={headerImage} alt="header" />
+    </div>
   );
 }
 
